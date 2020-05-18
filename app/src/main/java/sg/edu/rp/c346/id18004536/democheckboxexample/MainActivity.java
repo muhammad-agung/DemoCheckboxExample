ba9432fb7a04e3cfb7cd32639274035890c1dab2
BuildTools <unconfigured@null.spigotmc.org>
2020-05-18 16:44:04 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18004536/democheckboxexample/MainActivity.java
@@ -0,0 +1,65 @@
+package sg.edu.rp.c346.id18004536.democheckboxexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbEnabled;
+    Button btnCheck;
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cbEnabled = findViewById(R.id.checkBox);
+        btnCheck = findViewById(R.id.button);
+        tvShow = findViewById(R.id.textView);
+
+
+
+        btnCheck.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Log.i("MyActivity", "Inside onClick()");
+                Toast.makeText(MainActivity.this,"Button click",Toast.LENGTH_LONG).show();
+                if(cbEnabled.isChecked()) {
+                    double pay = calcPay(100, 20);
+                    tvShow.setText("The discount is given. You need to pay " + pay);
+                }
+                else {
+                    double pay = calcPay(100, 0);
+                    tvShow.setText("The discount is not given. You need to pay " + pay);
+                }
+
+                /*if(cbEnabled.isChecked()){
+                    tvShow.setText("The discount is given.");
+                }
+                else{
+                    tvShow.setText("The discount is not given.");
+                }*/
+
+            }
+
+        });
+    }
+
+    private double calcPay(double price, double discount) {
+        double pay = price * (1 - discount / 100);
+        return pay;
+
+    }
+
+
+    }
